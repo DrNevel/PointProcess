6de3cbc892f61b8d21d9710d13557a58e865fb55
Martina <mandorlini.martina@gmail.com>
2022-03-03 17:34:37 +0100

    Edit TriTest to use get_features_multi

--- TriTest.m
+++ TriTest.m
@@ -17,20 +17,20 @@ fpth=fullfile(pth,f);
 % about 17 seconds
 % tic
 % Features_RR=get_features_RR(fpth,'sel_EKGR',1,'fs',250,'PPorderMono',9,'get_PP',1,'UndSampl',20,'get_spectra',1,'get_TimeDomain',1,'get_Freqs',1,'get_Compl',1);
-Features_RR=get_features_RR(fpth,'sel_EKGR',1,'fs',250,'PPorderMono',8,'get_PP',1,'UndSampl',20,'get_spectra',1);
+Features_RR=get_features_multi(fpth,'sel_EKGR',1,'fs',250,'PPorderMono',8,'get_PP',1,'UndSampl',20,'get_spectra',1);
 % Features_RR=get_features_copy(fpth,'sel_EKGR',1,'fs',250,'PPorderMono',8,'get_PP',1,'get_mono',1,'UndSampl',20,'get_spectra',1);
 % toc
 
 % about 280 seconds
 % tic
 % Features_COV=get_features(fpth,'sel_EKGR',1,'fs',125,'PPorderBiva',7,'get_PP',1,'get_biva',1,'UndSampl',20,'get_spectra',1,'get_TimeDomain',1,'get_Freqs',1,'get_Compl',1);
-Features_COV=get_features(fpth,'sel_EKGR',1,'fs',250,'PPorderBiva',8,'get_PP',1,'get_biva',1,'UndSampl',20,'get_spectra',1);
+Features_COV=get_features_multi(fpth,'sel_EKGR',1,'fs',250,'PPorderBiva',8,'get_PP',1,'get_biva',1,'UndSampl',20,'get_spectra',1);
 % Features_COV=get_features_copy(fpth,'sel_EKGR',1,'fs',250,'PPorderBiva',8,'get_PP',1,'get_biva',1,'UndSampl',20,'get_spectra',1);
 % toc
 
 % about 610 seconds
 tic
-Features_TRI=get_trifeatures(fpth,'sel_EKGR',1,'fs',250,'PPorderMulti',8,'get_PP',1,'get_multi',1,'UndSampl',20,'get_spectra',1,'cov_type','pat-syst','correct_diast',1);
+Features_TRI=get_features_multi(fpth,'sel_EKGR',1,'fs',250,'PPorderMulti',8,'get_PP',1,'get_multi',1,'UndSampl',20,'get_spectra',1,'cov_type','pat-syst','correct_diast',1);
 %Features_TRI=get_features_copy(fpth,'sel_EKGR',1,'fs',250,'PPorderMulti',8,'get_PP',1,'get_multi',1,'UndSampl',20,'get_spectra',1,'cov_type','pat-syst');
 toc
 
